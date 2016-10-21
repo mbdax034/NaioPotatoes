6f8e5b68502ac813f1080c3a3adb36a42b9d29db
abdullah <abderabh@gmail.com>
2016-10-21 09:50:00 +0200

    delete files

--- README.md
+++
@@ -1,8 +0,0 @@
-# ApiClient
-ApiClient Skeleton. Tutorial purpose.
-
-Mandatory action :
-
-- You must clone ApiCodec into ext directory before compiling.
-
-
--- ext/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdullah/Documents/imerir/ROBO NAIO/API_NAIO/ext
-# Build directory: /home/abdullah/Documents/imerir/ROBO NAIO/API_NAIO/ext
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(ApiCodec)
