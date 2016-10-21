05e463548c88a9fa0918c484c977d60f66f0b3bf
abdullah <abderabh@gmail.com>
2016-10-21 09:47:11 +0200

    delete files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdullah/Documents/imerir/ROBO NAIO/API_NAIO
-# Build directory: /home/abdullah/Documents/imerir/ROBO NAIO/API_NAIO
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(ext)
