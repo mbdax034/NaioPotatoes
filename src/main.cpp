3616accd64a2e3179d95540ddad33dd29a11b340
Jeremy <barbe.jeremy66@hotmail.fr>
2016-10-26 11:09:56 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,16 @@
 #include "Core.hpp"
-#include "robot.hpp"
-
-#include <fstream>
-
-using namespace std;
-
+#include <sys/types.h>
 
 int main(int argc, char** argv ) {
 	// core initialisation
 	Core* core = new Core();
-	
-	printNull();
+    
 	// start main core thread
 	core->init( DEFAULT_HOST_ADDRESS, static_cast<uint16_t>( PORT_ROBOT_MOTOR ) ) ;
-
-	 // waits the thread exits
+    
+	// waits the thread exits
 	core->joinMainThread() ;
 
-	ofstream file;
-    file.open("test.text", ios::out | ios::app);
-    file << "je teste";
-    file.close();
 	delete core ;
 
 	return 0 ;
